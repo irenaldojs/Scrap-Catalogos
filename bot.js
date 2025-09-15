f155732a1ec3961562273615dcc9a28910b1cda0
irenaldo junior da silva <75049621+irenaldojs@users.noreply.github.com>
2025-09-15 20:56:43 +0000

    arteb

--- bot.js
+++ bot.js
@@ -1,129 +1,73 @@
-const fs = require("fs")
 const axios = require("axios")
 const puppeteer = require("puppeteer");
 
-const API_URL = 'http://localhost:3001/ds';
+const API_URL = 'http://localhost:3001/arteb';
+const CATALOGO_URL = 'https://www.arteb.com.br/catalogo/?pa_linha=leve';
 
 (async () => {
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(0);
 
-    const linha = "Sensor de Velocidade (VSS)"
-    const res = await GET_LINHA(linha)
-    const produtos = res.data
-    const passosTotais = produtos.length
+    const produtos = []
 
+    const pageTogal = 27
+    
+    for (let i=1; i < 2; i++){
+        console.log("passo :", i)
 
-    for (i = 0; i < passosTotais; i++) {
-        console.log(linha, "passo:", String(Number(i+1) + "/"+passosTotais));
+        await page.goto(CATALOGO_URL + "/page/" + i,  { waitUntil: 'load' })
+        const selector = 'ul.products.columns-4 > li';
+        await page.waitForSelector(selector);
 
-        await page.goto(produtos[i].link, { waitUntil: 'load' });
-        await page.waitForSelector('body > main > article > section.detalhes > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(3) > div > div > table > tbody');
+        const listaProdutos = await page.evaluate((sel) => {
+            // Retorna a quantidade de itens encontrados.
+            const elementos = document.querySelectorAll(sel);
+            
+            // Você também pode extrair informações de cada elemento, como o título do produto.
+            const produtos = Array.from(elementos).map(li => {
+                const titulo = li.querySelector('h2.woocommerce-loop-product__title').innerText.split(' – ');
+                const link = li.querySelector('a.woocommerce-LoopProduct-link').href;
+                const imagemElement = li.querySelector('div.thumbnail_container > img');
+                const imgSrc = imagemElement.getAttribute('data-src') || imagemElement.getAttribute('src');
+                const id = titulo.pop();
+                const referencia = titulo.join(" ");
+                return { id, referencia , link, image: imgSrc }; 
+            });
 
-        const equivalentes = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('body > main > article > section.detalhes > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(3) > div > div > table > tbody > tr'))
-                .map(tr => tr.innerText.trim())
-        });
+            return produtos;
+            
+        }, selector);
 
-        const aplicacoes = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('body > main > article > section.detalhes > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(4) > div > div > table > tbody > tr'))
-                .map(tr => tr.innerText.trim().split("\t"))
-        });
-
-        const formataAplicacoes = aplicacoes.map(aplicacao => {
-            let versao = (aplicacao[2] + (aplicacao[3] ? " " + aplicacao[3] : "") + (aplicacao[5] ? " " + aplicacao[5] : "")).replaceAll("-", "");
-            versao = versao == " " ? "" : versao;
-
-            return {
-                montadora: aplicacao[0],
-                modelo: aplicacao[1],
-                ano: aplicacao[4].replaceAll(" ", "").replace(">", "/").replaceAll("-", ""),
-                versao
-            }
-        })
-
-        const imagens = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll('body > main > article > section.detalhes > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div > div > div > ul > li > img'))
-                .map(img => img.src)
-        })
-
-
-        const atributos = await page.evaluate(() => {
-            const span = document.querySelector("body > main > article > section.detalhes > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2) > div > p > span");
-            return span ? span.innerText.trim().split("|") : null;
-        });
-
-        const atributosFormatados = atributos.map(item => item.replaceAll(': ', ':').replace(/^\s+|\s+$/g, ""))
-
-        const data = {
-            ...produtos[i],
-            //descricao: atributosFormatados[0].split(":")[1],
-            equivalentes,
-            aplicacoes: formataAplicacoes,
-            imagens,
-            atributos: atributosFormatados
-        }
-
-        //console.log(data)
-        PUT(data);
+        listaProdutos.forEach(produto => produtos.push(produto));
     }
 
-    console.log(`Finalizando ${produtos.length} registros.`)
+    for(let i = 0; i < produtos.length; i++){
+        await page.goto(produtos[i].link, { waitUntil: 'load' });
+
+        const selector2 = '#tab-description > p:nth-child(2)';
+        await page.waitForSelector(selector2);
+
+        const info = await page.evaluate((sel) => {
+            const elementos = document.querySelectorAll(sel);
+            
+            const infoArray = Array.from(elementos).map(item => {
+                // Seletores para os elementos
+                const montadoraElement = item.querySelector("tr.woocommerce-product-attributes-item--attribute_pa_montadora a"); 
+                               
+                // Verifique se o elemento existe antes de tentar acessar 'innerText'
+                const montadora = montadoraElement ? montadoraElement.innerText : null;
+                
+                return { montadora };
+            });
+
+            return infoArray;
+        }, selector2);
+        
+        console.log(info);
+    }
+
     await browser.close();
 })();
 
 
-async function POST(data) {
-
-    try {
-        const response = await axios.post(API_URL, data);
-        console.log('Dados enviados com sucesso:', response.data);
-    } catch (error) {
-        console.error('Erro ao enviar dados:', error);
-    }
-
-}
-async function PUT(data) {
-
-    try {
-        const response = (await axios.put(API_URL + "/" + data.id, data));
-        //console.log('Dados enviados com sucesso:', response.data);
-    } catch (error) {
-        console.error('Erro ao enviar dados:', error.message);
-    }
-
-}
-
-async function GETALL() {
-    try {
-        const response = (await axios.get(API_URL));
-        return response
-    } catch (error) {
-        console.error('Erro ao enviar dados:', error)
-        return error
-    }
-
-}
-
-async function GET_ID(id) {
-    try {
-        const response = await (await axios.get(API_URL + "/" + id));
-        console.log('Dados enviados com sucesso:', response);
-        return response
-    } catch (error) {
-        console.error('Erro ao enviar dados:', error)
-        return error
-    }
-
-}
-
-async function GET_LINHA(linha) {
-    try {
-        const response = await (await axios.get(API_URL + "?linha=" + linha));
-        return response
-    } catch (error) {
-        console.error('Erro ao enviar dados:', error)
-        return error
-    }
-}
